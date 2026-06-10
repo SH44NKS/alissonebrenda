eac72223ccfa94b61a6480babca5f8c83bd417a8
SH44NKS <alissons.silva25@gmail.com>
2026-06-10 18:18:46 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = "https://wxsscnqingirryawnomo.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_zhRabC6LRf2huklBcU_wPw_05Mhnsk1";
-export const COUPLE_ID = "alisson-brenda";
